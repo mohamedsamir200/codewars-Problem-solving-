c2cae15b6a23f218974e6505d0b0c3fb1e44dc6f
mohamedsamir200 <ms8113453@gmail.com>
2025-03-15 13:54:46 +0200

    update

--- main.js
+++ main.js
@@ -725,7 +725,19 @@
 // console.log(findSmallestInt([34, -345, -1, 100]));
 //#endregion
 
-//#region
+//#region Is n divisible by x and y  
+// Link ==> https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript 
+// function isDivisible(n, x, y) {
+//     // if (n % x == 0 && n % y == 0) {
+//     //     return true;
+//     // } else {
+//     //     return false;
+//     // }
+//     "=======================";
+//    return n % x == 0 && n % y == 0 ?true : false;
+// }
+// console.log(isDivisible(12, 3, 4));
+// console.log(isDivisible(5, 3, 4));
 //#endregion
 
 //#region
