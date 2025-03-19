bfb0c3e5e822a59f8691bf4f9320d45405ac943c
mohamedsamir200 <ms8113453@gmail.com>
2025-03-19 13:48:32 +0200

    update

--- main.js
+++ main.js
@@ -792,8 +792,11 @@
     //   console.log(makeUpperCase ("hello")) // HELLO
 //#endregion
 
-//#region 
-    //Link ==> 
-        
+//#region Array plus array
+    // //Link ==> https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/javascript
+    // function arrayPlusArray(arr1, arr2) {
+    //     return   [...arr1 , ...arr2].reduce( (acc , current) => acc + current , 0 )
+    //   }
+    //   console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])) // 21
 //#endregion
 
