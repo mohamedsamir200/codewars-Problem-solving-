9ca9ec804fe71f289f9d4d7c722eeb8a92bf6c64
mohamedsamir200 <ms8113453@gmail.com>
2025-04-01 13:24:44 +0200

    update

--- main.js
+++ main.js
@@ -895,8 +895,13 @@
 //#endregion
 
 
-//#region 
-    // Link ==> 
+//#region  Twice as old
+    // Link ==> https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+    // function twiceAsOld(dadYearsOld, sonYearsOld) {
+    //   return Math.abs ( dadYearsOld - (sonYearsOld * 2) )
+    // }
+    //   console.log(twiceAsOld(36, 7)) // 22
+    //   console.log(twiceAsOld(55,30)) // 5
 //#endregion
 
 //#region 
