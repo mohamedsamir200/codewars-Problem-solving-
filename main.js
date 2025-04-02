8f0313d2a2200411ec62bcc2bd2917c4f6fe9c64
mohamedsamir200 <ms8113453@gmail.com>
2025-04-02 13:07:21 +0200

    update

--- main.js
+++ main.js
@@ -925,3 +925,33 @@
 // console.log(correct("51NGAP0RE")); // S1NGAP0RE
 //#endregion
 
+//#region How good are you really?
+    // Link ==> https://www.codewars.com/kata/5601409514fc93442500010b/train/javascript
+    // function betterThanAverage(classPoints, yourPoints) {
+    //    const classPoint_avg = classPoints.reduce( (acc , current) => acc + current , 0) / classPoints.length 
+    //    return yourPoints >  classPoint_avg
+    // }
+    // console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)) // true 
+    // console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)) // false 
+
+//#endregion
+
+//#region 
+    // Link ==> 
+
+//#endregion
+
+//#region 
+    // Link ==> 
+
+//#endregion
+
+//#region 
+    // Link ==> 
+
+//#endregion
+
+//#region 
+    // Link ==> 
+
+//#endregion
