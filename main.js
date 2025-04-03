79aca98da2e3cd588b9aee89d9d1ea92434ed0d3
mohamedsamir200 <ms8113453@gmail.com>
2025-04-03 14:28:57 +0200

    update

--- main.js
+++ main.js
@@ -1025,8 +1025,14 @@
     //   console.log(expressionMatter(5, 1, 3)) // 6  
 //#endregion
 
-//#region  
-    // Link ==> 
+//#region  Is it even
+    // Link ==> https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript 
+    // function testEven(n) {
+    //     return n % 2 == 0
+    // }
+    // console.log(testEven(0)) // true 
+    // console.log(testEven(0.5)) // false 
+    // console.log(testEven(-4)) // false 
 //#endregion
 
 //#region  
