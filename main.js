ddfb05fa0e26bb3782f2d7d7cc2babd4342b6f5e
mohamedsamir200 <ms8113453@gmail.com>
2025-04-03 14:15:59 +0200

    update

--- main.js
+++ main.js
@@ -1000,9 +1000,15 @@
     // console.log(sumStr("2","")) // 5 
 //#endregion
 
-//#region  
-    // Link ==> 
-//#endregion
+// //#region  Sort and Star
+//     // Link ==> https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/javascript 
+//     function twoSort(s) {
+//         return s.sort()[0].split("").join("***")
+//     }
+//     console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
+//     // 'b***i***t***c***o***i***n'
+
+// //#endregion
 
 //#region  
     // Link ==> 
