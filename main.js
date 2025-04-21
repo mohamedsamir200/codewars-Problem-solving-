68cc2c811e8b05688619884b6a72a990b0f50953
mohamedsamir200 <ms8113453@gmail.com>
2025-04-21 15:52:56 +0200

    update

--- main.js
+++ main.js
@@ -1173,3 +1173,34 @@
 // console.log(billboard("Hadufuns John", 20)); // chars 13 * Price 20 =  260
 
 //#endregion
+
+//#region  Welcome to the city
+// Link ==> https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+// function sayHello(name, city, state) {
+//     return `Hello, ${name.join(" ")}! Welcome to ${city} , ${state}!`
+// }
+// console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')) 
+// // Hello, John Smith! Welcome to Phoenix, Arizona!
+// console.log(sayHello(['Wallace','Russel','Osbourne'],'Albany','New York')) 
+
+//#endregion
+
+//#region
+// Link ==>
+
+//#endregion
+
+//#region
+// Link ==>
+
+//#endregion
+
+//#region
+// Link ==>
+
+//#endregion
+
+//#region
+// Link ==>
+
+//#endregion
